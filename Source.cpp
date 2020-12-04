7f1ead9338958644735fff9b0c18dc9d0c54653b
Nik <vip.kuz2002@gmail.com>
2020-12-04 22:25:38 +0500

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+template <typename t1>
+
+void FillArray(t1 *arr, int n) 
+{
+	for (int i = 0; i < n; i++) 
+	{
+		arr[i] = rand();
+		cout << arr[i] << "\t";
+	}
+	cout << endl << endl;
+}
+
+template <typename t2>
+int Solve(t2 *arr, int n) 
+{
+	int k = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] % 2 == 0)
+		{
+			k++;
+		}
+	}
+	return k;
+}
+int main() 
+{
+	setlocale(LC_ALL, "rus");
+	srand(time(0));
+	int n;
+	cout << "Введите длину массивов\t";
+	cin >> n;
+	cout << endl;
+	int* arr = new int[n];
+	short int* arr1 = new short int[n];
+	long int* arr2 = new long int[n];
+
+	FillArray(arr, n);
+	FillArray(arr1, n);
+	FillArray(arr2, n);
+	cout << endl << endl;
+	
+
+	cout << "Первый: " << Solve(arr, n) << endl;
+	cout << "Второй: " << Solve(arr1, n) << endl;
+	cout << "Третий: " << Solve(arr2, n) << endl;
+	
+	delete[] arr;
+	delete[] arr1;
+	delete[] arr2;
+	
+	return 0;
+}
